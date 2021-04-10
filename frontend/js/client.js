6664939199ff74ce81bdc2ce243717a50aa12adb
Kyle Nguyen <nghia002@outlook.com>
2021-04-10 14:29:32 +0800

    setup: set up envoy proxy. Can communicate between web client and server using grpc

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -1,11 +1,11 @@
 const { HelloRequest } = require('./model/helloworld_pb.js');
 const { GreeterClient } = require('./model/helloworld_grpc_web_pb.js');
 
-var greeterClient = new GreeterClient('http://localhost:50051');
+var greeterClient = new GreeterClient('http://localhost:8080');
 // TODO: seems like js code can't connect directly to the gRPC server but has to go through a proxy
 // https://github.com/grpc/grpc-web/tree/master/net/grpc/gateway/examples/helloworld
-var request = new HelloRequest('you')
-// request.set
+var request = new HelloRequest();
+request.setName('You');
 console.log("test client");
 
 greeterClient.sayHello(request, {}, (err, response) => {
